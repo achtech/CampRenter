a6bb12aaf3996045f43333931740026e99f6cb56
Brahim Barjali <brahim.barjali@gmail.com>
2020-09-17 16:08:58 +0100

    - test

--- app/Models/EquipmentCategories.php
+++ app/Models/EquipmentCategories.php
@@ -7,6 +7,6 @@ class EquipmentCategories extends Base
     protected $table = 'equipment_categories';
     public $primarykey = 'id';
     protected $fillable = [
-        'label',
+        'label'
     ];
 }
