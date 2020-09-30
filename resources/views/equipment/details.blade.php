22fdd3fdffb48c93e6c108ff712302f226aa9f8a
Brahim Barjali <brahim.barjali@gmail.com>
2020-09-30 14:13:20 +0100

    feature/CAM-6-equipment-managment

--- resources/views/equipment/details.blade.php
+++ resources/views/equipment/details.blade.php
@@ -464,17 +464,27 @@
             </div>
 
             <div class="tab-pane" id="bookings">
-                                        <p>Food truck quinoa dolor sit amet, consectetuer adipiscing elit. Aenean
-                                            commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-                                            magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-                                            ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-                                            quis enim.</p>
-                                        <p class="mb-0">Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-                                            arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam
-                                            dictum felis eu pede mollis pretium. Integer tincidunt.Cras dapibus. Vivamus
-                                            elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-                                            porttitor eu, consequat vitae, eleifend ac, enim.</p>
-                                    </div>
+                
+            
+
+            <div class="table-responsive">
+                        <table id="default_order" class="table table-striped table-bordered display no-wrap"
+                            style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>{{ __('backend.client_name.lbl') }}</th>
+                                    <th>{{ __('backend.client_equipments.lbl') }}</th>
+                                    <th>{{ __('backend.client_rents.lbl') }}</th>
+                                </tr>
+                            </thead>
+                            
+                        </table>
+                    </div>
+
+
+
+
+            </div>
         </div>
 
     </div> <!-- end card-body-->
