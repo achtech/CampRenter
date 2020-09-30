502924aa8297661827ee89bab2953c42c8aacd25
Oumaima STITINI <oumaima.stitini@exo-it.com>
2020-09-30 17:14:49 +0100

    master

--- resources/views/transmission/index.blade.php
+++ resources/views/transmission/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layout', ['activePage' => 'insurance', 'titlePage' => __('backend.insurance.lbl')])
+@extends('layout', ['activePage' => 'transmission', 'titlePage' => __('backend.insurance.lbl')])
 @section('content')
 <div class="page-breadcrumb">
                 <div class="row">
