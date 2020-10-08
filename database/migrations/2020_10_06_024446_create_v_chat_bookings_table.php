19d92ad7baa7887c2b983890efb7935d7defeca8
Achraf Saloumi <achraf.saloumi@exo-it.com>
2020-10-08 11:56:28 +0100

    mas

--- database/migrations/2020_10_06_024446_create_v_chat_bookings_table.php
+++ database/migrations/2020_10_06_024446_create_v_chat_bookings_table.php
@@ -11,43 +11,7 @@ class CreateVChatBookingsTable extends Migration
      */
     public function up()
     {
-        DB::statement("CREATE VIEW v_chats_bookings AS "
-            . "SELECT"
-            . "`m`.`message` AS `message`,"
-            . "`m`.`date_sent` AS `date_sent`,"
-            . "`m`.`id_bookings` AS `id_bookings`,"
-            . "`m`.`ordre_message` AS `ordre_message`,"
-            . "`c`.`id` AS `id_owner`,"
-            . "`c`.`client_name` AS `owner_first_name`,"
-            . "`c`.`client_last_name` AS `owner_last_name`,"
-            . "`ao`.`image` AS `image_owner`,"
-            . "`l`.`id` AS `id_renter`,"
-            . "`l`.`client_name` AS `renter_first_name`,"
-            . "`l`.`client_last_name` AS `renter_last_name`,"
-            . "`ar`.`image` AS `image_renter`"
-            . "FROM"
-            . "("
-            . "   ("
-            . "   ("
-            . "   ("
-            . "   ("
-            . "   `chats` `m`"
-            . "JOIN `bookings` `b` ON"
-            . "(`m`.`id_bookings` = `b`.`id`)"
-            . ")"
-            . "LEFT JOIN `clients` `c` ON"
-            . "(`m`.`id_owners` = `c`.`id`)"
-            . ")"
-            . "LEFT JOIN `avatars` `ao` ON"
-            . "(`c`.`id_avatars` = `ao`.`id`)"
-            . ")"
-            . "LEFT JOIN `clients` `l` ON"
-            . "(`m`.`id_renters` = `l`.`id`)"
-            . ")"
-            . "LEFT JOIN `avatars` `ar` ON"
-            . "(`l`.`id_avatars` = `ar`.`id`))"
-
-        );
+        DB::statement("CREATE VIEW `v_chats_bookings`  AS  select `m`.`message` AS `message`,`m`.`date_sent` AS `date_sent`,`m`.`id_bookings` AS `id_bookings`,`m`.`ordre_message` AS `ordre_message`,`c`.`id` AS `id_owner`,`c`.`client_name` AS `owner_first_name`,`c`.`client_last_name` AS `owner_last_name`,`ao`.`image` AS `image_owner`,`l`.`id` AS `id_renter`,`l`.`client_name` AS `renter_first_name`,`l`.`client_last_name` AS `renter_last_name`,`ar`.`image` AS `image_renter` from (((((`chats` `m` join `bookings` `b` on((`m`.`id_bookings` = `b`.`id`))) left join `clients` `c` on((`m`.`id_owners` = `c`.`id`))) left join `avatars` `ao` on((`c`.`id_avatars` = `ao`.`id`))) left join `clients` `l` on((`m`.`id_renters` = `l`.`id`))) left join `avatars` `ar` on((`l`.`id_avatars` = `ar`.`id`)))");
     }
 
     /**
--- database/migrations/2020_10_06_025457_create_v_bookings_details_table.php
+++ database/migrations/2020_10_06_025457_create_v_bookings_details_table.php
@@ -11,32 +11,7 @@ class CreateVBookingsDetailsTable extends Migration
      */
     public function up()
     {
-        DB::statement("CREATE VIEW v_bookings_details AS "
-            . "SELECT"
-            . "`b`.`id` AS `id`,"
-            . "`b`.`start_date` AS `start_date`,"
-            . "`b`.`end_date` AS `end_date`,"
-            . "TO_DAYS(`b`.`end_date`) - TO_DAYS(`b`.`start_date`) AS `bookingDay`,"
-            . "`ca`.`label_en` AS `camper_name_en`,"
-            . "`ca`.`label_de` AS `camper_name_de`,"
-            . "`ca`.`label_fr` AS `camper_name_fr`,"
-            . "`e`.`price_per_day` AS `price_per_day`,"
-            . "`c`.`id` AS `client_id`,"
-            . "`c`.`client_name` AS `client_name`,"
-            . "`c`.`client_last_name` AS `client_last_name`"
-            . "FROM"
-            . "("
-            . "  ("
-            . "  (`bookings` `b`"
-            . "JOIN `campers` `e`)"
-            . "JOIN `clients` `c`"
-            . ")"
-            . "JOIN `camper_names` `ca`"
-            . ")"
-            . "WHERE"
-            . "`b`.`id_campers` = `e`.`id` AND `b`.`id_clients` = `c`.`id` AND `e`.`id_camper_names` = `ca`.`id`"
-
-        );
+        DB::statement("CREATE  VIEW `v_bookings_details`  AS  select `b`.`id` AS `id`,`b`.`start_date` AS `start_date`,`b`.`end_date` AS `end_date`,(to_days(`b`.`end_date`) - to_days(`b`.`start_date`)) AS `bookingDay`,`b`.`total` AS `total`,`b`.`status_booking` AS `status_booking`,`b`.`status_billings` AS `status_billings`,`ca`.`label_en` AS `camper_name_en`,`ca`.`label_de` AS `camper_name_de`,`ca`.`label_fr` AS `camper_name_fr`,`e`.`image` AS `image`,`e`.`price_per_day` AS `price_per_day`,`co`.`id` AS `owner_id`,`co`.`client_name` AS `owner_name`,`co`.`client_last_name` AS `owner_last_name`,`cr`.`id` AS `renter_id`,`cr`.`client_name` AS `renter_name`,`cr`.`client_last_name` AS `renter_last_name`,`com`.`rate` AS `commissions_rate`,`p`.`rate` AS `promotion_rate` from ((((((`bookings` `b` left join `commissions` `com` on((`com`.`id` = `b`.`id_commissions`))) left join `promotions` `p` on((`p`.`id` = `b`.`id_promotions`))) left join `campers` `e` on((`e`.`id` = `b`.`id_campers`))) left join `clients` `co` on((`co`.`id` = `b`.`id_clients`))) left join `clients` `cr` on((`cr`.`id` = `b`.`id_clients`))) left join `camper_names` `ca` on((`ca`.`id` = `e`.`id_camper_names`)))");
     }
 
     /**
