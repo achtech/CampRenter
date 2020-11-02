c5d198739b0e5601ee3914d65e3084e2fbbe13a7
Brahim Barjali <brahim.barjali@gmail.com>
2020-11-02 14:25:55 +0100

    master

--- resources/views/frontend/layout2.blade.php
+++ resources/views/frontend/layout2.blade.php
@@ -52,7 +52,7 @@
 					<ul id="responsive">
 
 					<li><a class="{{ $activePage == 'home' ? ' current' : '' }}" href="{{route('home.index')}}">{{trans('front.menu_home')}}</a></li>
-					<li><a  href="#">{{trans('front.menu_rent')}}</a>
+					<li><a class="{{ $activePage == 'campersearch' ? ' current' : '' }}" href="#">{{trans('front.menu_rent')}}</a>
 							<ul>
 								@foreach($categories as $cat)
 									<li><a href="{{route('frontend.camper.search')}}">{{App\Http\Controllers\Controller::getLabelFromObject($cat)}}</a></li>
