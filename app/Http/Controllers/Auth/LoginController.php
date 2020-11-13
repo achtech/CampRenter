610e8855efc9564230d32369dc999eee2985d31e
Brahim Barjali <brahim.barjali@gmail.com>
2020-11-13 12:40:31 +0100

    master

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-use Auth;
 use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
@@ -20,7 +20,7 @@ class LoginController extends Controller
     | redirecting them to your home screen. The controller uses a trait
     | to conveniently provide its functionality to your applications.
     |
-    */
+     */
     use AuthenticatesUsers;
     protected $redirectTo = RouteServiceProvider::HOME;
     public function __construct()
@@ -35,15 +35,15 @@ class LoginController extends Controller
             ['is_confirmed', 1],
             ['availability', 2],
         ])->get();
-        $blogs =  DB::table('blogs')->orderBy('created_at','desc')->get();
-        return view('frontend.auth.login')->with('blogs', $blogs)->->with('campers', $campers);
+        $blogs = DB::table('blogs')->orderBy('created_at', 'desc')->get();
+        return view('frontend.auth.login')->with('blogs', $blogs)->with('campers', $campers);
     }
 
     public function adminLogin(Request $request)
     {
-       $this->validate($request, [
-            'email'   => 'exists:clients|required|email',
-            'password' => 'required|min:6'
+        $this->validate($request, [
+            'email' => 'exists:clients|required|email',
+            'password' => 'required|min:6',
         ]);
 
         if (Auth::guard('client')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
