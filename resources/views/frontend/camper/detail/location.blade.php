23eb03df796dc499b19c3451cc1a5c003526eabd
Brahim Barjali <brahim.barjali@gmail.com>
2020-12-07 23:07:41 +0100

    add payment invoice and fix detail map

--- resources/views/frontend/camper/detail/location.blade.php
+++ resources/views/frontend/camper/detail/location.blade.php
@@ -1,41 +1,45 @@
 <h3 class="listing-desc-headline margin-top-60 margin-bottom-30">{{trans('front.location')}}</h3>
 
-<div id="singleListingMap-container">
-    <div class="markers-on-the-map">
-        <!-- Map -->
-        <div id="map_canvas"></div>
-    </div>
-    <input style="display: none" type="text" name="position_x" id="currentLatitude">
-    <input style="display: none" type="text" name="position_y" id="currentLongitude">
-</div>
-<script type="text/javascript">
-	var mapD;
-    function initialize() {
-        var mapOptionsD = {
-            center: new google.maps.LatLng('{{$camper->position_x}}', '{{$camper->position_y}}'),
-            zoom: 9,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var el=document.getElementById("map_canvas");
-        mapD = new google.maps.Map(el, mapOptionsD);
-        var markerD
-        // var marker = new google.maps.Marker({
-        //     map: map,
-        //     position: latlng
-        // });
 
-        var sunCircle = {
-            strokeColor: "#c3fc49",
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: "#c3fc49",
-            fillOpacity: 0.35,
-            map: mapD,
-            center: new google.maps.LatLng('{{$camper->position_x}}', '{{$camper->position_y}}'),
-            radius: 15000 // in meters
-        };
-        cityCircle = new google.maps.Circle(sunCircle);
-        cityCircle.bindTo('center', markerD, 'position');
-    }
-    initialize();
+<div id="map_canvas" style="width: 400px; height: 400px;"></div>
+
+<script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfCVL7v7EJXFy70y3vF9mb_AusJlhg0H4"
+></script>
+<script type="text/javascript">
+	var map;
+var latlng = new google.maps.LatLng('{{$camper->position_x}}','{{$camper->position_y}}');
+function initialize() {
+    var mapOptions = {
+        center: latlng,
+        zoom: 9,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var el=document.getElementById("map_canvas");
+    map = new google.maps.Map(el, mapOptions);
+
+    /**var marker = new google.maps.Marker({
+        map: map,
+        position: latlng
+    });*/
+
+    var sunCircle = {
+        strokeColor: "#c3fc49",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#c3fc49",
+        fillOpacity: 0.35,
+        map: map,
+        center: latlng,
+        radius: 15000 // in meters
+    };
+    cityCircle = new google.maps.Circle(sunCircle);
+    cityCircle.bindTo('center', marker, 'position');
+}
+
+$ = jQuery.noConflict();
+$(function( $ ) {
+initialize();
+
+});
 </script>
