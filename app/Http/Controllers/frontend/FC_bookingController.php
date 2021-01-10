ddc4f02dd716b80a767ab072613e0c43eb312f11
Brahim Barjali <brahim.barjali@gmail.com>
2021-01-10 13:32:48 +0100

    defoxed_env

--- app/Http/Controllers/frontend/FC_bookingController.php
+++ app/Http/Controllers/frontend/FC_bookingController.php
@@ -152,7 +152,7 @@ class FC_bookingController extends Controller
                         ->orWhere('end_month', $sMonth);
                 })
                 ->first();
-            $total = ($pricePerDay ? $pricePerDay->price_per_night : ''+$insurance) * $nbrDays;
+            $total = ($pricePerDay ? $pricePerDay->price_per_night : 0 + $insurance) * $nbrDays;
         } else {
             $nbrDays1 = Controller::diffDate($start_date, date("Y-m-t", strtotime($start_date))); //15/08/2021->31/08/2021
             $nbrDays2 = Controller::diffDate(date("Y-m-01", strtotime($end_date)), $end_date); //01/09/2021->02/09/2021
@@ -173,7 +173,7 @@ class FC_bookingController extends Controller
                 })
                 ->first()->price_per_night;
 
-            $total = ($pricePerDay1 ? $pricePerDay1->price_per_night : ''+$insurance) * ($nbrDays1 + 1) + ($pricePerDay2 ? $pricePerDay2->price_per_night : ''+$insurance) * $nbrDays2;
+            $total = ($pricePerDay1 ? $pricePerDay1->price_per_night : 0 + $insurance) * ($nbrDays1 + 1) + ($pricePerDay2 ? $pricePerDay2->price_per_night : 0 + $insurance) * $nbrDays2;
         }
         return $total;
     }
