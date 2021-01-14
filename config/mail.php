6b2255f3d3704623348b8edc677e20cb788f35cf
Brahim Barjali <brahim.barjali@gmail.com>
2021-01-14 15:55:48 +0100

    defoxed_env

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
      */
 
-    'default' => env('MAIL_MAILER', 'gmail'),
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
