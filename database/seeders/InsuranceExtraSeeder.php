a5277c0c0cb7b89b97597037dfc979fa34bfd3e0
Achraf Saloumi <achraf.saloumi@exo-it.com>
2021-01-22 02:03:06 +0100

    add insrance and extra to database

---
+++ database/seeders/InsuranceExtraSeeder.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Seeder;
+
+class InsuranceExtraSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('insurance_extra')->insert(array(
+            0 => array(
+                'id' => 1,
+                'name' => "Innenraumversicherung",
+                'nbr_days_from' => 1,
+                'nbr_days_to' => 1,
+                'initial_price' => 25,
+                'price_per_day' => 3,
+                'created_at' => date("Y-m-d h:i:s"),
+                'updated_at' => date("Y-m-d h:i:s")
+            ),
+            1 => array(
+                'id' => 2,
+                'name' => "Assistance",
+                'nbr_days_from' => 0,
+                'nbr_days_to' => 10,
+                'initial_price' => 16,
+                'price_per_day' => 4,
+                'created_at' => date("Y-m-d h:i:s"),
+                'updated_at' => date("Y-m-d h:i:s")
+            ),
+            2 => array(
+                'id' => 3,
+                'name' => "Assistance",
+                'nbr_days_from' => 11,
+                'nbr_days_to' => 1000,
+                'initial_price' => 56,
+                'price_per_day' => 1,
+                'created_at' => date("Y-m-d h:i:s"),
+                'updated_at' => date("Y-m-d h:i:s")
+            ),
+            3 => array(
+                'id' => 4,
+                'name' => "InsassenunfallLight",
+                'nbr_days_from' => 1,
+                'nbr_days_to' => 1,
+                'initial_price' => 10,
+                'price_per_day' => 1,
+                'created_at' => date("Y-m-d h:i:s"),
+                'updated_at' => date("Y-m-d h:i:s")
+            ),
+            4 => array(
+                'id' => 5,
+                'name' => "InsassenunfallPLUS",
+                'nbr_days_from' => 1,
+                'nbr_days_to' => 1,
+                'initial_price' => 20,
+                'price_per_day' => 2,
+                'created_at' => date("Y-m-d h:i:s"),
+                'updated_at' => date("Y-m-d h:i:s")
+            ),
+            5 => array(
+                'id' => 6,
+                'name' => "Bonusverlust & Selbsbtbehalt",
+                'nbr_days_from' => 1,
+                'nbr_days_to' => 1,
+                'initial_price' => 7.5,
+                'price_per_day' => 2.5,
+                'created_at' => date("Y-m-d h:i:s"),
+                'updated_at' => date("Y-m-d h:i:s")
+            )
+        ));
+    }
+}
